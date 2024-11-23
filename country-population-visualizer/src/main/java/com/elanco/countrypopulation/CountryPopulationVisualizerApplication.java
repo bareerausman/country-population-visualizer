5145c40fb0e7457ff056fe746b5511208f884ef9
bareerausman <bareera.usman12@icloud.com>
2024-11-23 18:03:37 +0000

    setting up

---
+++ country-population-visualizer/src/main/java/com/elanco/countrypopulation/CountryPopulationVisualizerApplication.java
@@ -0,0 +1,13 @@
+package com.elanco.countrypopulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryPopulationVisualizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryPopulationVisualizerApplication.class, args);
+	}
+
+}
---
+++ country-population-visualizer/src/test/java/com/elanco/countrypopulation/CountryPopulationVisualizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elanco.countrypopulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryPopulationVisualizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
